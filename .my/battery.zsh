d2a0161ad3f7f086cde5979f610513b92457acb7
Ron Martinez <ramartin.net@gmail.com>
2016-05-21 00:56:01 -0400

    WIP

--- .my/battery.zsh
+++ .my/battery.zsh
@@ -18,9 +18,9 @@ battery_indicator() {
 
   local battery_percent=$(echo $(pmset -g batt) | grep --only-matching '[0-9]\+%')
   local battery_number=${battery_percent:0:-1}
-  local scaled_battery=$((battery_number * slots_total / 100.0))
-  local slots_filled=$(printf %.0f scaled_battery)
-  local slots_empty=$((slots_total - slots_filled))
+  local scaled_battery=$(($battery_number * $slots_total / 100.0))
+  local slots_filled=$(printf %.0f "$scaled_battery")
+  local slots_empty=$(($slots_total - $slots_filled))
   local indicator_filled=$(printf "$symbol_filled%.0s" $(seq 1 $slots_filled))
   local indicator_unfilled=$(printf "$symbol_unfilled%.0s" $(seq 1 $slots_empty))
 
