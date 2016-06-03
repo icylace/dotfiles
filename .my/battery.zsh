6d2b34d84ffefb1539f3e061c7644137939a6b80
Ron Martinez <ramartin.net@gmail.com>
2016-06-03 19:02:05 -0400

    WIP

--- .my/battery.zsh
+++ .my/battery.zsh
@@ -12,25 +12,29 @@ battery_indicator() {
   local slots_total=10
   local symbol_filled='✿'
   local symbol_unfilled='❀'
-
   local threshold_medium=60
   local threshold_low=40
 
   local battery_percent=$(echo $(pmset -g batt) | grep --only-matching '[0-9]\+%')
   local battery_number=${battery_percent:0:-1}
-  local scaled_battery=$(($battery_number * $slots_total / 100.0))
-  local slots_filled=$(printf %.0f "$scaled_battery")
-  local slots_empty=$(($slots_total - $slots_filled))
-  local indicator_filled=$(printf "$symbol_filled%.0s" $(seq 1 $slots_filled))
-  local indicator_unfilled=$(printf "$symbol_unfilled%.0s" $(seq 1 $slots_empty))
 
-  local color=''
-  if [ $battery_number -gt $threshold_medium ] ; then
+  local scaled_battery=$((battery_number * slots_total / 100.0))
+  local slots_filled=$(printf %.0f "$scaled_battery")
+  local slots_empty=$((slots_total - slots_filled))
+  local indicator_filled=''
+  local indicator_unfilled=''
+  if ((slots_filled > 0)) ; then
+    indicator_filled=$(printf "$symbol_filled%.0s" $(seq 1 $slots_filled))
+  fi
+  if ((slots_empty > 0)) ; then
+    indicator_unfilled=$(printf "$symbol_unfilled%.0s" $(seq 1 $slots_empty))
+  fi
+
+  local color='red'
+  if ((battery_number > threshold_medium)) ; then
     color='green'
-  elif [ $battery_number -gt $threshold_low ] ; then
+  elif ((battery_number > threshold_low)) ; then
     color='yellow'
-  else
-    color='red'
   fi
 
   echo "%{$fg[$color]%}$indicator_filled$indicator_unfilled%{$reset_color%}"
