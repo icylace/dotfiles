adf638056a3131272be56757f62cce6e2d6fee02
Ron Martinez <ron@pantheon.io>
2016-08-23 22:08:40 -0400

    WIP

--- .my/git.sh
+++ .my/git.sh
@@ -185,3 +185,32 @@ setup_git_commands_that_pretty_print() {
 }
 
 setup_git_commands_that_pretty_print
+
+
+# ------------------------------------------------------------------------------
+
+
+# http://stackoverflow.com/questions/1286183/git-find-fat-commit#comment19133031_1557398
+# https://github.com/sschuberth/dev-scripts/blob/master/git/git-commit-size.sh
+git-commit-size() {
+  if [ $# -ne 1 ] ; then
+    echo "Rationale : Get the total size of all files added by the specified commit."
+    echo "Usage     : git-commit-size <commit>"
+    return 1
+  fi
+
+  blob_sizes=$(git diff-tree -r -c -M -C --no-commit-id $1 | grep " A " | cut -d " " -f 4 | git cat-file --batch-check | cut -d " " -f 3)
+  total_size=0
+
+  for size in $blob_sizes ; do
+    let total_size+=size
+  done
+
+  if [ $total_size -gt 0 ] ; then
+    total_size_kib=$(echo $total_size | awk '{printf "%.2f",$1/1024}')
+    total_size_mib=$(echo $total_size_kib | awk '{printf "%.2f",$1/1024}')
+    echo "The commit adds files totalling in $total_size bytes ($total_size_kib KiB, $total_size_mib MiB)."
+  else
+    echo "The commit does not add any files."
+  fi
+}
