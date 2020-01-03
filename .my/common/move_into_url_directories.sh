a3c1d8faec9c2d0f46cddf4b6bcf2943e819dd3e
Ron Martinez <ramartin.net@gmail.com>
2020-01-03 13:31:53 -0500

    WIP

--- .my/common/move_into_url_directories.sh
+++ .my/common/move_into_url_directories.sh
@@ -57,58 +57,59 @@ move_into_url_directories() {
 
 
 # TODO:
-folder_name_from_url() {
-  local -a origins path_parts urls
-  local dir_path file_entry origin path_part url
 
-  if [ ! -f "$1" ] ; then
-    return
-  fi
+# folder_name_from_url() {
+#   local -a origins path_parts urls
+#   local dir_path file_entry origin path_part url
 
-  origins=($(mdls -name kMDItemWhereFroms -nullMarker '' -raw "$1"))
+#   if [ ! -f "$1" ] ; then
+#     return
+#   fi
 
-  if [ -z "$origins" ] ; then
-    return
-  fi
+#   origins=($(mdls -name kMDItemWhereFroms -nullMarker '' -raw "$1"))
+
+#   if [ -z "$origins" ] ; then
+#     return
+#   fi
 
 
-  local origin="#1"
+#   local origin="#1"
 
-  path_parts=()
+#   path_parts=()
 
-  for origin in "${origins[@]}" ; do
-    if [ -n "$origin" ] && [[ $origin =~ '[^()"]' ]] ; then
-      url="${origin#\"}"
-      url="${url%\"}"
-      url="${url%\",}"
-      # https://stackoverflow.com/a/20048220
-      url="$(echo $url | grep -iIohE 'https?://[^[:space:]]+')"
-      url="${url#http://}"
-      url="${url#https://}"
-      # https://stackoverflow.com/a/13298479
-      url="${url//:/::}"        # Handle URLs that use colons.
-      url="${url//\//:}"
-      if [ -n "$url" ] ; then
-        path_parts+=("$url")
-      fi
-    fi
-  done
+#   for origin in "${origins[@]}" ; do
+#     if [ -n "$origin" ] && [[ $origin =~ '[^()"]' ]] ; then
+#       url="${origin#\"}"
+#       url="${url%\"}"
+#       url="${url%\",}"
+#       # https://stackoverflow.com/a/20048220
+#       url="$(echo $url | grep -iIohE 'https?://[^[:space:]]+')"
+#       url="${url#http://}"
+#       url="${url#https://}"
+#       # https://stackoverflow.com/a/13298479
+#       url="${url//:/::}"        # Handle URLs that use colons.
+#       url="${url//\//:}"
+#       if [ -n "$url" ] ; then
+#         path_parts+=("$url")
+#       fi
+#     fi
+#   done
 
-  # https://stackoverflow.com/a/17758600
-  path_parts=($(printf "%q\n" "${path_parts[@]}" | sort --unique))
+#   # https://stackoverflow.com/a/17758600
+#   path_parts=($(printf "%q\n" "${path_parts[@]}" | sort --unique))
 
-  dir_path=''
+#   dir_path=''
 
-  for path_part in "${path_parts[@]}" ; do
-    dir_path="$path_part/$dir_path"
-  done
+#   for path_part in "${path_parts[@]}" ; do
+#     dir_path="$path_part/$dir_path"
+#   done
 
-  if [ -n "$dir_path" ] ; then
-    # TODO:
-    echo "$dir_path"
-    echo "$1"
-    # mkdir -pv "$dir_path"
-    # mv "$1" "$dir_path"
-  fi
+#   if [ -n "$dir_path" ] ; then
+#     # TODO:
+#     echo "$dir_path"
+#     echo "$1"
+#     # mkdir -pv "$dir_path"
+#     # mv "$1" "$dir_path"
+#   fi
 
-}
+# }
