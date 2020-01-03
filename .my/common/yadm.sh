b004ea895164db13d198ad51ec41c988bb783649
Ron Martinez <ramartin.net@gmail.com>
2020-01-03 13:03:44 -0500

    WIP

--- .my/common/yadm.sh
+++ .my/common/yadm.sh
@@ -7,7 +7,7 @@ alias yaa='yadm add'
 alias yad='yadm diff --histogram --word-diff=color HEAD'
 alias yad1='yadm diff --histogram --word-diff=color HEAD~1 HEAD'
 alias yal='yadm log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"'
-alias yap='yadm pull --rebase=preserve origin master'
+alias yap='yadm pull --rebase=merges origin master'
 alias yas='yadm list -a'
 alias yay='yadm add --update && yadm commit -m "WIP" && yadm push --set-upstream origin master'
 
