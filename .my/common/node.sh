6813c4fa6c30e74c171c1fade14e5fc055a431af
Ron Martinez <ramartin.net@gmail.com>
2020-04-28 23:50:22 -0400

    wip

--- .my/common/node.sh
+++ .my/common/node.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# https://dev.to/sijils/publishing-npm-package-to-github-package-repository-5ane
+alias npml="npm login --registry=https://npm.pkg.github.com/ --scope=@icylace"
+
 setup_node() {
   local nvm_file="$(brew --prefix nvm)/nvm.sh"
   if [ -f "$nvm_file" ] ; then
