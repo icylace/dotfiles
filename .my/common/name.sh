03e23570b1a64d08ad5978fe91f3037447c1a244
Ron Martinez <ramartin.net@gmail.com>
2020-05-07 02:53:12 -0400

    wip

--- .my/common/name.sh
+++ .my/common/name.sh
@@ -1,79 +1,31 @@
 #!/usr/bin/env bash
 
-# TODO:
-# - Is it possible to make this less overkill?
-
 # Checks if a potential a name for a new alias/function is taken already.
 name() {
-  local brightRed='\e[1;31m'
   local blue='\e[0;34m'
   local reset='\e[0m'
 
-  local taken="${brightRed}Name is taken!${reset}"
+  echo "${blue}type $1${reset}"
+  type $1
 
-  type $1 > /dev/null 2>&1
-  if [ $? -eq 0 ] ; then
-    echo $taken
-    echo "${blue}type $1${reset}"
-    echo $(type $1)
-    return 1
-  fi
+  echo "${blue}whence $1${reset}"
+  echo $(whence $1)
 
-  whence $1 > /dev/null 2>&1
-  if [ $? -eq 0 ] ; then
-    echo $taken
-    echo "${blue}whence $1${reset}"
-    echo $(whence $1)
-    return 1
-  fi
+  echo "${blue}where $1${reset}"
+  echo $(where $1)
 
-  where $1 > /dev/null 2>&1
-  if [ $? -eq 0 ] ; then
-    echo $taken
-    echo "${blue}where $1${reset}"
-    echo $(where $1)
-    return 1
-  fi
+  echo "${blue}which $1${reset}"
+  echo $(which $1)
 
-  which $1 > /dev/null 2>&1
-  if [ $? -eq 0 ] ; then
-    echo $taken
-    echo "${blue}which $1${reset}"
-    echo $(which $1)
-    return 1
-  fi
+  echo "${blue}brew info $1${reset}"
+  echo $(brew info $1)
 
-  brew info $1 > /dev/null 2>&1
-  if [ $? -eq 0 ] ; then
-    echo $taken
-    echo "${blue}brew info $1${reset}"
-    echo $(brew info $1)
-    return 1
-  fi
+  echo "${blue}brew cask info $1${reset}"
+  echo $(brew cask info $1)
 
-  brew cask info $1 > /dev/null 2>&1
-  if [ $? -eq 0 ] ; then
-    echo $taken
-    echo "${blue}brew cask info $1${reset}"
-    echo $(brew cask info $1)
-    return 1
-  fi
+  echo "${blue}cargo search $1${reset}"
+  echo $(cargo search $1)
 
-  cargo search $1 > /dev/null 2>&1
-  if [ $? -eq 0 ] ; then
-    echo $taken
-    echo "${blue}cargo search $1${reset}"
-    echo $(cargo search $1)
-    return 1
-  fi
-
-  gem search $1 > /dev/null 2>&1
-  if [ $? -eq 0 ] ; then
-    echo $taken
-    echo "${blue}gem search $1${reset}"
-    echo $(gem search $1)
-    return 1
-  fi
-
-  echo Name is not taken.
+  echo "${blue}gem search $1${reset}"
+  echo $(gem search $1)
 }
